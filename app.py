5dcc60904cfabf65855657941920ba302d3896c1
urimanul <myopera9@gmail.com>
2024-11-21 05:34:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ def runQuery(query):
 def getDatabaseSchema():
     return st.session_state.db.get_table_info() if st.session_state.db else "Please connect to database"
 
-llm = Groq.ChatGroq(model="Llama-3.1-70b-Versatile", api_key="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")
+llm = Groq.ChatGroq(
+    model="Llama-3.1-70b-Versatile",
+    api_key="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"
+)
 #llm = Groq(
     #api_key="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"
 #)
